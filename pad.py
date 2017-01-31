dc534795db5e41cb1a3ba85744243e6e21f8dc15
cthomp18 <code8593@gmail.com>
2017-01-31 15:21:39 -0800

    start

---
+++ pad.py
@@ -0,0 +1,22 @@
+def ascii_to_hex ( ascii_text ):
+   hex_text = ascii_text.encode("hex");
+   #print(hex_text)
+   return hex_text
+   
+def hex_to_ascii ( hex_text ):
+   ascii_text = hex_text.decode("hex");
+   #print(ascii_text)
+   return ascii_text
+   
+def hex_to_base64 ( hex_text ):
+   base64_text = hex_text.decode("hex").encode("base64");
+   #print(base64_text)
+   return base64_text
+   
+def base64_to_hex ( base64_text ):
+   hex_text = base64_text.decode("base64").encode("hex");
+   #print(hex_text)
+   return hex_text
+   
+def pad( text ):
+   return None
